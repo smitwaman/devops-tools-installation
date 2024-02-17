23f5d64ce11fbb3b1601c7d7e3e170a3ae225d81
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-17 07:47:15 +0530

    Update Maven.sh

--- Maven.sh
+++ Maven.sh
@@ -1,3 +1,11 @@
 sudo apt update -y
 sudo apt install maven -y
 mvn -version
+
+vi /etc/profile.d/maven.sh
+ -------------------------------------------------
+  export MAVEN_HOME=/opt/apache-maven-3.6.3
+  export PATH=$PATH:$MAVEN_HOME/bin
+ -------------------------------------------------
+source /etc/profile.d/maven.sh
+mvn -version
