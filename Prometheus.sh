310e6e4d502590451a92ce7a330d0fab201cc3aa
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-19 11:51:44 +0530

    Update Prometheus.sh

--- Prometheus.sh
+++ Prometheus.sh
@@ -1,1 +1,18 @@
-docker run --name prometheus -d -p 127.0.0.1:9090:9090 prom/prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.34.0/prometheus-2.34.0.linux-amd64.tar.gz
+tar -xzf prometheus-2.34.0.linux-amd64.tar.gz
+sudo mv prometheus-2.34.0.linux-amd64 /usr/local/prometheus
+sudo useradd -rs /bin/false prometheus
+sudo chown -R prometheus:prometheus /usr/local/prometheus
+cd /usr/local/prometheus
+sudo nano prometheus.yml
+
+# global:
+#   scrape_interval: 15s
+
+# scrape_configs:
+#  - job_name: 'prometheus'
+#    static_configs:
+#      - targets: ['localhost:9090']
+
+cd /usr/local/prometheus
+sudo -u prometheus ./prometheus --config.file=prometheus.yml
