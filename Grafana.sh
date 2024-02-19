37b85e4a2ca7cdc63f8068f80887451aaf609b57
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-19 11:56:36 +0530

    Update Grafana.sh

--- Grafana.sh
+++ Grafana.sh
@@ -1,1 +1,6 @@
-docker run -d -p 3000:3000 --name=grafana grafana/grafana-enterprise
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"Add the Grafana GPG key:wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -Update 
+sudo apt-get update
+sudo apt-get install grafanaStart 
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
