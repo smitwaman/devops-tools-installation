153c9f1ba3beb3c3c3a9f0ae225c8a041108373b
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-19 11:41:13 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,1 +1,19 @@
- docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+#Run using sudo -i
+apt install docker-compose 
+
+apt install unzip
+
+adduser sonarqube
+
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip
+
+unzip sonarqube-9.4.0.54424.zip 
+
+chmod -R 755 sonarqube-9.4.0.54424
+
+chown -R sonarqube:sonarqube sonarqube-9.4.0.54424
+
+cd sonarqube-9.4.0.54424/bin/linux-x86-64/
+
+./sonar.sh start
+
