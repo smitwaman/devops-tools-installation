466047136fbc07815fe5cf3e554c22bf8c7f8c9b
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-19 11:57:13 +0530

    Update Grafana.sh

--- Grafana.sh
+++ Grafana.sh
@@ -1,5 +1,6 @@
 sudo apt-get install -y software-properties-common
-sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"Add the Grafana GPG key:wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -Update 
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -Update 
 sudo apt-get update
 sudo apt-get install grafanaStart 
 sudo systemctl start grafana-server
