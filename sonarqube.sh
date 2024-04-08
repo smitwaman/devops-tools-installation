6023e9d402ad38dac17ecb234b6a14ae8d0dfe18
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-04-08 08:49:02 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,29 +1,102 @@
-#Run using sudo -i
-apt install docker-compose 
+Virtual Machine running Ubuntu 22.04 or newer
 
-apt install unzip
+Install Postgresql 15
+sudo apt update
+sudo apt upgrade
 
-adduser sonarqube
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip
+wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
 
-unzip sonarqube-9.4.0.54424.zip 
+sudo apt update
+sudo apt-get -y install postgresql postgresql-contrib
+sudo systemctl enable postgresql
+Create Database for Sonarqube
+sudo passwd postgres
+su - postgres
 
-chmod -R 755 sonarqube-9.4.0.54424
+createuser sonar
+psql 
+ALTER USER sonar WITH ENCRYPTED password 'sonar';
+CREATE DATABASE sonarqube OWNER sonar;
+grant all privileges on DATABASE sonarqube to sonar;
+\q
 
-chown -R sonarqube:sonarqube sonarqube-9.4.0.54424
+exit
+Install Java 17
+sudo bash
 
-cd sonarqube-9.4.0.54424/bin/linux-x86-64/
+apt install -y wget apt-transport-https
+mkdir -p /etc/apt/keyrings
 
-./sonar.sh start
+wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
 
-# three steps
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
 
-apt install unzip
-adduser sonarqube
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip
-unzip *
-chmod -R 755 /home/sonarqube/sonarqube-9.4.0.54424
-chown -R sonarqube:sonarqube /home/sonarqube/sonarqube-9.4.0.54424
-cd sonarqube-9.4.0.54424/bin/linux-x86-64/
-./sonar.sh start
+apt update
+apt install temurin-17-jdk
+update-alternatives --config java
+/usr/bin/java --version
+
+exit 
+Increase Limits
+sudo vim /etc/security/limits.conf
+Paste the below values at the bottom of the file
+
+sonarqube   -   nofile   65536
+sonarqube   -   nproc    4096
+sudo vim /etc/sysctl.conf
+Paste the below values at the bottom of the file
+
+vm.max_map_count = 262144
+Reboot to set the new limits
+
+sudo reboot
+Install Sonarqube
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.0.65466.zip
+sudo apt install unzip
+sudo unzip sonarqube-9.9.0.65466.zip -d /opt
+sudo mv /opt/sonarqube-9.9.0.65466 /opt/sonarqube
+sudo groupadd sonar
+sudo useradd -c "user to run SonarQube" -d /opt/sonarqube -g sonar sonar
+sudo chown sonar:sonar /opt/sonarqube -R
+Update Sonarqube properties with DB credentials
+
+sudo vim /opt/sonarqube/conf/sonar.properties
+Find and replace the below values, you might need to add the sonar.jdbc.url
+
+sonar.jdbc.username=sonar
+sonar.jdbc.password=sonar
+sonar.jdbc.url=jdbc:postgresql://localhost:5432/sonarqube
+Create service for Sonarqube
+
+sudo vim /etc/systemd/system/sonar.service
+Paste the below into the file
+
+[Unit]
+Description=SonarQube service
+After=syslog.target network.target
+
+[Service]
+Type=forking
+
+ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
+ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
+
+User=sonar
+Group=sonar
+Restart=always
+
+LimitNOFILE=65536
+LimitNPROC=4096
+
+[Install]
+WantedBy=multi-user.target
+Start Sonarqube and Enable service
+
+sudo systemctl start sonar
+sudo systemctl enable sonar
+sudo systemctl status sonar
+sudo tail -f /opt/sonarqube/logs/sonar.log
+Access the Sonarqube UI
+http://<IP>:9000
